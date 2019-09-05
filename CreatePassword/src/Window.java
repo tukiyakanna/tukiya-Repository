02e0f8652cfae7e6deaf3d3cc7f9f09e5a64c1b1
tukiya_kanna <kaminashitukiya@gmail.com>
2019-09-05 16:24:32 +0900

    555

--- CreatePassword/src/Window.java
+++ CreatePassword/src/Window.java
@@ -4,18 +4,22 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Window extends JFrame implements ActionListener
 {
 	JPanel [] panel;
+	JTextField jtf;
 
 	Window()
 	{
 		panel = new JPanel[2];
 		setSize(500,500);
 		setResizable(false);
-		add(panel[0] = new JPanel(),new GridLayout(2,1));
+		add(panel[0] = new JPanel());
+		panel[0].setLayout(new  GridLayout(2,1));
 		panel[0].add(panel[1] = new JPanel(),new GridLayout(1,2));
+		panel[1].add(jtf = new JTextField(1));
 	}
 
 	@Override
--- CreatePassword/src/createpass.java
+++ CreatePassword/src/createpass.java
@@ -14,7 +14,7 @@ public class createpass {
 		String[] str = {"1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
 				"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","-","_"};
 
-		int Length = 4;//パスワードの長さを決めてね
+		int Length = 5;//パスワードの長さを決めてね
 		String pass = "";
 		Random rnd = new Random();
 		int pos;
