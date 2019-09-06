fc1e41afb431199bc36f23f1eba413254227786a
tukiya_kanna <kaminashitukiya@gmail.com>
2019-09-06 12:26:50 +0900

    555

--- CreatePassword/src/Window.java
+++ CreatePassword/src/Window.java
@@ -2,6 +2,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,25 +11,26 @@ public class Window extends JFrame implements ActionListener
 {
 	JPanel [] panel;
 	JTextField jtf;
+	JCheckBox pool_select;
 
 	String[] A = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 	String[] num = {"1","2","3","4","5","6","7","8","9","0"};
-	String[] a;
+	String[] a,pool;
 
 	Window()
 	{
-		array_set();
+		tolower();
 		panel = new JPanel[3];
 		setSize(300,200);
 		setResizable(false);
 		add(panel[0] = new JPanel());
 		panel[0].setLayout(new  GridLayout(2,1));
-		panel[0].add(panel[1] = new JPanel(),new GridLayout(1,3));
+		panel[0].add(panel[1] = new JPanel(),new GridLayout(1,4));
 		panel[0].add(panel[2] = new JPanel(),new GridLayout(1,2));
 		panel[1].add(jtf = new JTextField(2));
 	}
 
-	void array_set()
+	void tolower()
 	{
 		a = new String[A.length];
 		for(int i = 0;i<A.length;i++)
