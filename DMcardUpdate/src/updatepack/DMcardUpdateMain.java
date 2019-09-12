53f18501ab15f75eb764b5f6e40009ab1e2e5629
tukiya_kanna <kaminashitukiya@gmail.com>
2019-09-12 13:30:16 +0900

    555

--- DMcardUpdate/src/updatepack/DMcardUpdateMain.java
+++ DMcardUpdate/src/updatepack/DMcardUpdateMain.java
@@ -4,15 +4,20 @@ public class DMcardUpdateMain
 {
 	public static void main (String [] args)
 	{
-		CreateConnectStatement ccs = new CreateConnectStatement();
-		ReadCardText rct = new ReadCardText();
 		CheckFile cf = new CheckFile("newcard.txt");
-		
+
 		boolean exist = cf.check();
 		if(exist==true)
 		{
-			
+			CreateConnectStatement ccs = new CreateConnectStatement();
+			ReadCardText rct = new ReadCardText();
+
+		}
+		else
+		{
+			ErrorWindow ew = new ErrorWindow("ファイルが存在しません");
+			ew.setVisible(true);
 		}
 	}
-	
+
 }
--- DMcardUpdate/src/updatepack/ErrorWindow.java
+++ DMcardUpdate/src/updatepack/ErrorWindow.java
@@ -1,5 +1,21 @@
 package updatepack;
 
-public class ErrorWindow {
+import java.awt.Toolkit;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ErrorWindow extends JFrame
+{
+	JPanel base;
+	JLabel errorstring;
+
+	ErrorWindow(String error)
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(Toolkit.getDefaultToolkit().getScreenSize().width-500,Toolkit.getDefaultToolkit().getScreenSize().height-400,500,400);
+		add(base = new JPanel());
+		base.add(errorstring = new JLabel(error));
+	}
 }
