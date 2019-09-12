7a6d1ad04313cc6a24f338954e9ced9304c5b607
tukiya_kanna <kaminashitukiya@gmail.com>
2019-09-12 14:14:09 +0900

    555

--- DMcardUpdate/src/updatepack/Window.java
+++ DMcardUpdate/src/updatepack/Window.java
@@ -1,23 +1,34 @@
 package updatepack;
 
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Window extends JFrame implements ActionListener
 {
-	JPanel base;
+	JPanel base,buttonp;
+	JLabel[] label;
+	JButton[] button;
 	CreateConnectStatement css;
 	ReadCardText rct;
 
 	Window(int x,int y)
 	{
-		css = new CreateConnectStatement();
-		rct = new ReadCardText();
-		setBounds((Toolkit.getDefaultToolkit().getScreenSize().width/2)-x,(Toolkit.getDefaultToolkit().getScreenSize().height/2)-y,x,y);
+		setBounds((Toolkit.getDefaultToolkit().getScreenSize().width-x)/2,(Toolkit.getDefaultToolkit().getScreenSize().height-y)/2,x,y);
+		label = new JLabel[3];
+		button = new JButton[2];
+		add(base = new JPanel(),new GridLayout(2,1));
+		base.add(label[0] = new JLabel("<html>更新データがあります<br>更新しますか?</html>"));
+		label[0].setHorizontalAlignment(JLabel.CENTER);
+		add(buttonp = new JPanel(),new GridLayout(1,2));
+		buttonp.add(button[0] = new JButton("YES"));
+		buttonp.add(button[1] = new JButton("NO"));
 	}
 
 	@Override
