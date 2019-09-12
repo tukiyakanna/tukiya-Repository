736a505fd437f9089f3b2a970dd74d8475f5cde0
tukiya_kanna <kaminashitukiya@gmail.com>
2019-09-12 12:26:30 +0900

    555

--- DMcardUpdate/src/updatepack/DMcardUpdateMain.java
+++ DMcardUpdate/src/updatepack/DMcardUpdateMain.java
@@ -4,6 +4,6 @@ public class DMcardUpdateMain
 {
 	public static void main (String [] args)
 	{
-
+		
 	}
 }
---
+++ DMcardUpdate/src/updatepack/Window.java
@@ -0,0 +1,18 @@
+package updatepack;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame implements ActionListener
+{
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
